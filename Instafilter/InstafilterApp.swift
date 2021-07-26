de278ca8d7bf0305f4a4abde5a72d6d7855f33cb
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-26 23:50:20 +0100

    Initial Commit

---
+++ Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Bruce Gilmour on 2021-07-26.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
