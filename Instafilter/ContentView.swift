1b5f216842f2cdb9e657e18a048144067ceb937e
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-27 22:57:35 +0100

    Integrating Core Image with SwiftUI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -6,14 +6,60 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
     var body: some View {
-        ActionSheetView()
+        CoreImageTestView()
     }
 }
 
-struct ActionSheetView: View {
+struct CoreImageTestView: View {
+    @State private var image: Image?
+
+    var body: some View {
+        VStack {
+            image?
+                .resizable()
+                .scaledToFit()
+        }
+        .onAppear(perform: loadImage)
+    }
+
+    func loadImage() {
+        guard let inputImage = UIImage(named: "example") else { return }
+        let beginImage = CIImage(image: inputImage)
+
+        let context = CIContext()
+
+//        let currentFilter = CIFilter.sepiaTone()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.intensity = 1
+
+//        let currentFilter = CIFilter.pixellate()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.scale = 30
+
+//        let currentFilter = CIFilter.crystallize()
+//        currentFilter.inputImage = beginImage
+//        currentFilter.radius = 40
+
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        currentFilter.radius = 400
+        currentFilter.center = CGPoint(x: inputImage.size.width / 2, y: inputImage.size.height / 2)
+
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            image = Image(uiImage: uiImage)
+        }
+    }
+}
+
+struct ActionSheetTestView: View {
     @State private var showingActionSheet = false
     @State private var backgroundColor = Color.white
 
@@ -36,7 +82,7 @@ struct ActionSheetView: View {
     }
 }
 
-struct BlurTextView: View {
+struct BlurTextTestView: View {
     @State private var blurAmount: CGFloat = 0
 
     var body: some View {
