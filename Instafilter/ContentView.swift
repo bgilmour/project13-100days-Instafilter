93911d2168a68b521878161324dc3e3c45cd0347
Bruce Gilmour <bruce.gilmour@langtoun.com>
2021-07-28 01:13:46 +0100

    Building our basic UI

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -10,8 +10,51 @@ import CoreImage
 import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+
     var body: some View {
-        ImagePickerTestView()
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(Color.secondary)
+
+                    if image != nil {
+                        image?
+                            .resizable()
+                            scaledToFit()
+                    } else {
+                        Text("Tap to select a picture")
+                            .font(.headline)
+                            .foregroundColor(.white)
+                    }
+                }
+                .onTapGesture {
+                    // select an image
+                }
+
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter") {
+                        // change filter
+                    }
+
+                    Spacer()
+
+                    Button("Save") {
+                        // save the picture
+                    }
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationBarTitle("Instafilter")
+        }
     }
 }
 
